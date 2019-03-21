5b526528ca3a83f0a4bd0fc96482c8e81eae45fc
JHiltunen <joonas.s.jh@gmail.com>
2019-03-21 19:51:13 +0200

    Added possibility to drag the word, div elements

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ function init(ulkolaksy) {
         var div = document.createElement("DIV");
         div.id = "word" + index;
         div.innerText = word;
+
+        // make the element draggable
+        div.draggable = true;
+        // add event listener to the element to apply function to dragstart attribute
+        div.onload = function(event) {
+            this.addEventListener('dragstart', drag, false);
+        };
+
         document.getElementById("boxPile").appendChild(div);
 
         // increase the index by one
@@ -19,5 +27,11 @@ function init(ulkolaksy) {
     });
 
     console.log("All elements are now created to the page");
+}
 
+function drag(event) {
+    console.log("Drag started..")
+    // set data type and value of dragged data
+    // in this case type is "text" and the value is id of the draggable element (for example: "word0", "word1")
+    event.dataTransfer.setData("text", event.target.id);
 }
