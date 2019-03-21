4487f326560d365b98275206770ac43222eb5763
JHiltunen <joonas.s.jh@gmail.com>
2019-03-21 20:42:41 +0200

    Started implementing the drop feature

--- js/main.js
+++ js/main.js
@@ -26,7 +26,38 @@ function init(ulkolaksy) {
         index++;
     });
 
-    console.log("All elements are now created to the page");
+    console.log("All individual word elements are now created to the page");
+    
+    // call function to create div elements where user is able to drag words
+    initBoxSlots(ulkolaksy);
+}
+
+function initBoxSlots(ulkolaksy) {
+    console.log("Starting to create elements for boxSlots")
+    var index = 0;
+    
+    // loop through the individual words to make as many slots as there are words
+    ulkolaksy.forEach(element => {
+        var boxSlot = document.createElement("DIV");
+        boxSlot.id = "slot" + index;
+        boxSlot.innerText = "slot";
+
+        // add event listeners to the element to apply function to on dragover and ondrop attributes
+        boxSlot.onload = function(event) {
+            this.addEventListener('onclick', function() {
+                console.log("CLicked");
+            });
+            this.addEventListener('ondragover', allowDrop(event), false);
+            this.addEventListener('ondrop', drop(event), false);
+        }
+
+        document.getElementById("boxPile").appendChild(boxSlot);
+
+        // increase the index by one
+        index++;
+    });
+
+    console.log("All boxSlots are now created on the page");
 }
 
 function drag(event) {
@@ -34,4 +65,17 @@ function drag(event) {
     // set data type and value of dragged data
     // in this case type is "text" and the value is id of the draggable element (for example: "word0", "word1")
     event.dataTransfer.setData("text", event.target.id);
+}
+
+function allowDrop(event) {
+    // by default, data/elements cannot be dropped in other elements.
+    // To allow a drop, we must prevent default handling of the element
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault(); // prevent the browser default handling of the data (default is open as link on drop)
+    var data = event.dataTransfer.getData("text"); // Get the dragged data with the dataTransfer.getData() method. In this case it's the id of individual word
+    console.log("Data: " + data); // The dragged data is the id of the dragged element ("word1")
+    event.target.appendChild(document.getElementById(data)); // Append the dragged element into the drop element
 }
