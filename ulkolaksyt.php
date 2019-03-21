adb6b5b7838e572db64d908f969a8b39651b6d64
JHiltunen <joonas.s.jh@gmail.com>
2019-03-21 21:05:57 +0200

    Removed extra whitespaces

--- ulkolaksyt.php
+++ ulkolaksyt.php
@@ -9,14 +9,16 @@
         // loop until file pointer reaches end of file
         while (! feof($file)) {
             // replace dots with spaces
-            $row = str_replace(".", " ", fgets($file));
+            $row = trim(str_replace(".", " ", fgets($file)));
+            // remove double spaces
+            $row = str_replace("  ", " ", $row);
             // convert string to array using PHP function
             $array = explode(" ", $row);
         }
 
         // close file
         fclose($file);
-
+        
         // shuffle the array so words are not in correct order on UI
         shuffle($array);
 
