b027695af16f6c7ac05909f6bf58020a5e7e7ae5
Joonas Hiltunen <joonas.s.jh@gmail.com>
2019-06-20 17:20:11 +0300

    Changed the UI so that user doesn't need to drag elements to boxes. Now using JQuery

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="js/main.js"></script>
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <title>Ulkoläksy peli</title>
@@ -20,11 +22,15 @@
 
             // pass ulkoläksy to init function so we can create elements for individual words
             init(array);
+    
+            $(function() {
+                $( "#boxPile" ).sortable();
+                $( "#boxPile" ).disableSelection();
+            } );
         });
     </script>
 
     <div id="boxPile"></div>
-    <div id="boxSlots"></div>
 
     <p class="answer"></p>
 
