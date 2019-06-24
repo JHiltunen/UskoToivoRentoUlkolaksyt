72916a9eca0af3afbd534ca70e5ff8ef39b530b6
Joonas Hiltunen <joonas.s.jh@gmail.com>
2019-06-24 10:48:24 +0300

    Added drag&drop support for mobile

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="js/jquery.ui.touch-punch.min.js"></script>
+
     <script src="js/main.js"></script>
     <script src="js/nav.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
